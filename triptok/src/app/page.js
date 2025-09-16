2bc9bb5455ab865a263dadff17c36dae69387ae2
Riana-S <rianasantos808@gmail.com>
2025-09-16 13:54:21 -1000

    Header format created
    App title and categories menu

--- triptok/src/app/page.js
+++ triptok/src/app/page.js
@@ -1,40 +1,20 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className="{stlyes.container}">
-          <h1
-            style={{
-              fontSize: "3rem",
-              fontWeight: "bold",
-              letterSpacing: "2px",
-              color: "#4f46e5",
-              textShadow: "2px 2px 6px rgba(0,0,0,0.2)",
-              marginBottom: "1rem",
-            }}
-          >
-            Travel Time ✈️
-          </h1>
-
-          <ol>
-            <li>Welcome to the base website for Travel Time.</li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a className={styles.primary} href="#">
-                Deploy now
-            </a>
-            <a className={styles.secondary} href="#">
-              Read our docs
-            </a>
+    <main>
+      <header>
+        <h1 className="title">TripTime</h1>
+        <div className="dropdown">
+          <button className="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#013d5a" className="bi-caret-down-fill" viewBox="0 0 16 16">
+  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+</svg>  Categories</button>
+          <div className="dropdown-content">
+            <a href="#">Culture</a>
+            <a href="#">Eat & Drink</a>
+            <a href="#">Activities</a>
           </div>
         </div>
-      </main>
-
-    <footer className={styles.footer}>Footer content here</footer>
-  </div>
+      </header>
+      <p>This should render with the Sigmar One font.</p>
+    </main>
   );
 }
